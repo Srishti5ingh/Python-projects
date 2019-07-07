d54b0b1fd2d4268ced16b54ccc830850a807257e
Srishti5ingh <40942227+Srishti5ingh@users.noreply.github.com>
2019-07-07 22:24:11 +0530

    Delete uri_manipulation.py

--- uri_manipulation.py
+++
@@ -1,6 +0,0 @@
-import uriModule
-print(" \n Enter any of the following numbers to get parts of the URI/URL : ")
-n = input(" 1 for scheme \n 2 for sub-domain \n 3 for domain \n 4 for directory/path \n 5 for sub-directory/query \n 6 for page/fragment\n\n ")
-
-uriModule.uriManipulation(n)
-
